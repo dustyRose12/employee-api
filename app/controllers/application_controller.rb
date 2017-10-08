89fe30d86d0427a611678c1ff139440a1070e2a1
dustyRose12 <dustyrose125@gmail.com>
2017-10-08 13:05:55 -0500

    employee-api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session #we changed this to null_session
+
+  before_action :restrict_access
+
+private #this makes the restrict_method private so you can't access it outside
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |api_key, token|
+      User.find_by(api_key: api_key, email: request.headers['X-User-Email'])
+    end
+  end
 end
